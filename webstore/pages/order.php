7b603823df73318fd838add1150ffd3771dbb698
Phuong0210 <phuong.ho021021@gmail.com>
2022-03-19 10:00:59 +0700

    done

--- webstore/pages/order.php
+++ webstore/pages/order.php
@@ -10,7 +10,15 @@
 </head>
 <body>
 
-
+<form action="">
+            <label for="bdaymonth">Select (month and year):</label>
+            <input type="month" id="bdaymonth" name="bdaymonth">
+            <div>
+                <input type="range" id="money" name="money" min="500000" max="200000000">
+                <label for="volume">Money</label>
+            </div>
+            <input type="submit">
+        </form>
 
 </body>
 <script src="/assets/javascript/app.js"></script>
